78d126c88c737f395cbd7dfc2ce86e95b395333a
rifqiziyad <rifqiimtinan@gmail.com>
2021-05-12 11:32:11 +0700

    setup folder structures

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const compression = require('compression')
+const bodyParser = require('body-parser')
+const routerNavigation = require('./routes/')
+
+const app = express()
+const port = 3001
+
+app.use(morgan('dev'))
+app.use(cors())
+app.options('*', cors())
+app.use(helmet())
+app.use(compression())
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+app.use('/api/v1', routerNavigation)
+app.use('/api', express.static('src/uploads'))
+
+app.listen(port, () => {
+  console.log(`Express app is listen on port ${port}`)
+})

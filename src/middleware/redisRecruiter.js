be5b45bac99741d77811a0a381e84a653de3d4d2
twicks95 <teguh.wck95@gmail.com>
2021-05-23 16:57:35 +0700

    Modify redisRecruiter.js

--- src/middleware/redisRecruiter.js
+++ src/middleware/redisRecruiter.js
@@ -5,11 +5,14 @@ const helper = require('../helpers/wrapper')
 module.exports = {
   getRecruiterByIdRedis: (req, res, next) => {
     const { id } = req.params
-    // console.log('get data by id redis' + id)
+
     client.get(`getrecruiter:${id}`, (error, result) => {
       if (!error && result != null) {
-        // console.log('data ada di redis')
-        return helper.response(res, 200, 'Succes Get Data By Id', JSON.parse(result)
+        return helper.response(
+          res,
+          200,
+          'Succes Get Data By Id',
+          JSON.parse(result)
         )
       } else {
         console.log('data tidak ada di redis')
@@ -18,18 +21,16 @@ module.exports = {
     })
   },
   getRecruiterRedis: (req, res, next) => {
-    // const { id } = req.params
-    // console.log('get data by id redis' + id)
     client.get(`getrecruiter:${JSON.stringify(req.query)}`, (error, result) => {
       if (!error && result != null) {
-        console.log('data ada di redis')
-        // console.log(JSON.parse(result))
         const newResult = JSON.parse(result) // {data,pageinfo}
-        return helper.response(res,
+        return helper.response(
+          res,
           200,
           'Succes Get Recruiter',
           newResult.result,
-          newResult.pageInfo)
+          newResult.pageInfo
+        )
       } else {
         console.log('data tidak ada di redis')
         next()
@@ -37,9 +38,7 @@ module.exports = {
     })
   },
   clearDataRecruiterRedis: (req, res, next) => {
-    // proses pertama mencari kunci yang berawalan getmovie
     client.keys('getrecruiter*', (_error, result) => {
-      // console.log(result) // bentuknya array ['getmovie']
       if (result.length > 0) {
         result.forEach((item) => {
           client.del(item)

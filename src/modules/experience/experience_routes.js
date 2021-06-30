d852b5ede6a9e16a2f4f8c19def8a23fc19ec9d6
twicks95 <teguh.wck95@gmail.com>
2021-06-30 21:05:42 +0700

    Update experience module

--- src/modules/experience/experience_routes.js
+++ src/modules/experience/experience_routes.js
@@ -1,16 +1,10 @@
 const express = require('express')
 const experienceController = require('./experience_controller')
 const Route = express.Router()
-const { sayHello } = require('./experience_controller')
-const uploadFile = require('../../middleware/uploads')
 
-Route.get('/hello', sayHello)
-
-Route.get('/hello', experienceController.sayHello)
-Route.get('/', experienceController.getExperience)
-Route.post('/', uploadFile, experienceController.postExperience)
+Route.post('/', experienceController.postExperience)
 Route.get('/:id', experienceController.getExperienceById)
-Route.patch('/:id', uploadFile, experienceController.updateExperience)
+Route.patch('/:id', experienceController.updateExperience)
 Route.delete('/:id', experienceController.deleteExperience)
 
 module.exports = Route

b4697c5951fe0815afdac06f82ecd1fa734463b5
twicks95 <teguh.wck95@gmail.com>
2021-06-30 21:06:11 +0700

    Update skill module

--- src/modules/skill/skill_routes.js
+++ src/modules/skill/skill_routes.js
@@ -1,16 +1,12 @@
 const express = require('express')
 const Route = express.Router()
-const { sayHello } = require('./skill_controller')
-
-const uploadFile = require('../../middleware/uploads')
+// const uploadFile = require('../../middleware/uploads')
 const skillControler = require('./skill_controller')
-Route.get('/hello', sayHello)
 
-Route.get('/hello', skillControler.sayHello)
 Route.get('/', skillControler.getSkill)
-Route.get('/:id/ling/:idd', skillControler.getSkillById)
+Route.get('/:id', skillControler.getSkillById)
 Route.get('/sort/ling', skillControler.getSkillBySort)
-Route.post('/', uploadFile, skillControler.postSkill)
+Route.post('/', skillControler.postSkill)
 Route.patch('/:id', skillControler.updateSkill)
 Route.delete('/:id', skillControler.deleteSkill)
 

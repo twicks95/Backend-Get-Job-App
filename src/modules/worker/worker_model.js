745601c50d9432ef5eb2f66851d93e411d71711c
rifqiziyad <rifqiimtinan@gmail.com>
2021-07-01 16:09:02 +0700

    fix search by skill name

--- src/modules/worker/worker_model.js
+++ src/modules/worker/worker_model.js
@@ -4,7 +4,7 @@ module.exports = {
   getDataAll: (limit, offset, search, sort) => {
     return new Promise((resolve, reject) => {
       connection.query(
-        `SELECT *, COUNT(*) AS jumlah_skill FROM workers JOIN skills ON workers.worker_id = skills.worker_id WHERE skills.skill_name LIKE '%${search}%' ORDER BY ${sort} LIMIT ${limit} OFFSET ${offset} `,
+        `SELECT *, COUNT(*) AS jumlah_skill FROM workers JOIN skills ON workers.worker_id = skills.worker_id WHERE skills.skill_name LIKE '%${search}%' GROUP BY workers.worker_id ORDER BY ${sort} LIMIT ${limit} OFFSET ${offset}`,
         (error, result) => {
           !error ? resolve(result) : reject(new Error(error))
         }
